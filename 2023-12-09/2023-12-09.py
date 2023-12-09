49c0d5988486bee1252d418180a1b6a93392b5ab
ArjanM83 <arjan@meulendyks.nl>
2023-12-09 08:01:56 +0100

    Added day 4

--- 2023-12-09/2023-12-09.py
+++ 2023-12-09/2023-12-09.py
@@ -7,10 +7,13 @@ class AdventOfCode:
         self.sum_of_puzzle_2 = 0
 
     @staticmethod
-    def get_differences_dict(line):
-        differences_dict = {0: [int(i) for i in line.split(' ')]}
-        line_id = 0
+    def get_differences_dict(line, reverse=False):
+        if reverse:
+            differences_dict = {0: [int(i) for i in line.split(' ')][::-1]}
+        else:
+            differences_dict = {0: [int(i) for i in line.split(' ')]}
 
+        line_id = 0
         while len([i for i in differences_dict[line_id] if i != 0]) > 0:  # while not all zero's
             line_id += 1
             previous_line_id = line_id - 1
@@ -24,28 +27,25 @@ class AdventOfCode:
 
         return differences_dict
 
+    @staticmethod
+    def get_increment(differences_dict):
+        increment = differences_dict[len(differences_dict) - 2][0]
+        for i in range(len(differences_dict) - 3, -1, -1):
+            last_number = differences_dict[i][-1]
+            increment = last_number + increment
+
+        return increment
+
     def solve_part_1(self):
         for line in self.lines_list:
-            differences_dict = self.get_differences_dict(line)
-
-            increment = differences_dict[len(differences_dict) - 2][0]
-            for i in range(len(differences_dict)-3, -1, -1):
-                last_number = differences_dict[i][-1]
-                increment = last_number + increment
-
+            increment = self.get_increment(self.get_differences_dict(line))
             self.sum_of_puzzle_1 += increment
 
         return f'\nResult puzzle 1: {self.sum_of_puzzle_1}\n'
 
     def solve_part_2(self):
         for line in self.lines_list:
-            differences_dict = self.get_differences_dict(line)
-
-            increment = differences_dict[len(differences_dict) - 2][0]
-            for i in range(len(differences_dict)-3, -1, -1):
-                first_number = differences_dict[i][0]
-                increment = first_number - increment
-
+            increment = self.get_increment(self.get_differences_dict(line, reverse=True))
             self.sum_of_puzzle_2 += increment
 
         return f'\nResult puzzle 2: {self.sum_of_puzzle_2}'
